029401f7ed2dd04cc27c1c370db6886b47dd22d6
yun <caseyunliu@gmail.com>
2023-07-17 11:09:23 -0400

    add deployed frontend url

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'https://www.yoursite.com',
+    'https://rococo-cuchufli-54741b.netlify.app',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000'
